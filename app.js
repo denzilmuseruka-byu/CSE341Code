04f5047a13246ba626daeccc7c043a5538df6130
Dirk Dhure <ext.dirk.dhure@veneka.com>
2022-02-06 21:34:29 +0200

    return html and routes using router module

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express'); 
+const path = require('path');
 
-const server = http.createServer(routes);
-server.listen(3000);
+const adminRoutes= require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+
+app.use((req,res,next)=>{
+res.status(404).sendFile(path.join(__dirname,'views', '404.html'));
+});
+
+app.listen(3000);
