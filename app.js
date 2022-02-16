65cc97268dcfa838936ffc5176a07c672ce3b6c6
Denzil M <denzilmuseruka@byui.edu>
2022-02-16 02:15:50 +0200

    use mongo

--- app.js
+++ app.js
@@ -2,23 +2,53 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-
+const User = require('./models/user');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
-
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findById('5bab316ce0a7c75f783cb8a8')
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+mongoose
+    .connect(
+        'mongodb+srv://denzilmuseruka@byui.edu:YY!ZWuWLT7Z2Xu@@cluster0.acj9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    )
+    .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: 'Denzil Museruka',
+                    email: 'denzilmuseruka@byui.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        });
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
